daae5321980efefc521c96941d451a610c6a0b2a
Igor Kagan <igor.b.kagan@gmail.com>
2018-10-30 15:36:28 +0100

    pre-SfN updates

--- testsim_comass_vs_vector_sum.m
+++ testsim_comass_vs_vector_sum.m
@@ -5,7 +5,8 @@
 % (see also: http://www.jneurosci.org/content/32/10/3339 https://link.springer.com/content/pdf/10.1007%2Fs004220050411.pdf )
 
 
-ScaleFactor = 50; % for plotting normalized length of the circular mean of the tuning curve
+if 0 % variant 1: 8 targets
+ScaleFactor = 10; % for plotting normalized length of the circular mean of the tuning curve
 
 target_positions2D_a =  [10    5 0  -5 -10  -5  0   5]; 
 target_positions2D_b =  [0.001 5 10 5 0.001 -5 -10 -5];
@@ -17,6 +18,27 @@ target_positions2D = complex(target_positions2D_a,target_positions2D_b);
 FR		=	5 + [2  10  2   0   0   0   0   0];
 FR = FR*2;
 
+elseif 1 % variant 2: 4 targets
+	ScaleFactor = 1; % for plotting normalized length of the circular mean of the tuning curve
+
+	target_positions2D_a =  [1  -1  -1  1]; 
+	target_positions2D_b =  [1   1	 -1  -1];
+	target_positions2D = complex(target_positions2D_a,target_positions2D_b); 
+	FR		=	1 * [1   0.1 0.1 0.1];
+	
+	
+elseif 0 % variant 3: 8 targets but same tuning as variant 2
+	ScaleFactor = 1; % for plotting normalized length of the circular mean of the tuning curve
+
+	target_positions2D_a =  [1  0 -1 -1 -1  0  1 1]; 
+	target_positions2D_b =  [1  1  1  0 -1 -1 -1 0];
+	target_positions2D = complex(target_positions2D_a,target_positions2D_b); 
+	FR		=	1 * [1 0.1285  0.1 0.0707 0.1 0.0707 0.1 0.1285];
+	
+end
+
+
+
 FRtarget =   FR.*target_positions2D./abs(target_positions2D);
 
 FRtarget_norm = ScaleFactor*sum(FRtarget)/sum(FR); % this is same as Ldir below! But it is invariant to gain!
@@ -26,12 +48,12 @@ Ldir_a = angle(sum(FR.*exp(i*angle(target_positions2D)))); % angle
 
 figure
 plot(FRtarget,'ko'); hold on; plot([FRtarget FRtarget(1)],'k-');
-line([0 (real(FRtarget_norm))],[0 (imag(FRtarget_norm))],'Color',[0 1 0],'LineWidth',3);
-line([0 mean(real(FRtarget))],[0 mean(imag(FRtarget))]);
+line([0 (real(FRtarget_norm))],[0 (imag(FRtarget_norm))],'Color',[0 1 0],'LineWidth',3); % it is not vector sum! it is normalized by sum(FR)
+line([0 mean(real(FRtarget))],[0 mean(imag(FRtarget))],'Color',[0 0 0],'LineWidth',1);
 
-polar(Ldir_a,Ldir,'mo');
+polar(Ldir_a,Ldir,'mo'); % it is not vector sum! it is normalized by sum(FR)
 
-[cx,cy] = comass(real(FRtarget),imag(FRtarget));
+[cx,cy] = ig_comass(real(FRtarget),imag(FRtarget));
 plot(cx,cy,'ro');
 axis equal
 title(sprintf('%s |mean(FRtarget)|=%.2f Ldir=%.2f Ldir/FRtarget=%.2f Ldir/comass=%.2f comass=%.2f',...
--- testsim_tuning_inactivation.m
+++ testsim_tuning_inactivation.m
@@ -3,15 +3,21 @@ function testsim_tuning_inactivation
 % variant 1: one task, two conditions (e.g. pre- and post-injection), two epochs: "baseline" and "response", 
 % four trial types (contralesional and ipsilesional hand and space)
 
-close all;
+% close all;
 
 % 1 - control
 %base1 = 10*ones(1,10); % [0:5:50]; % baseline firing
-base1 = 2:11;
-R1_csch = 5:5:50;
-R1_csih = 5:5:50;
-R1_isch = 5*ones(1,10);
-R1_isih = 5*ones(1,10);
+% base1 = 2:11;
+% R1_csch = 5:5:50;
+% R1_csih = 5:5:50;
+% R1_isch = 5*ones(1,10);
+% R1_isih = 5*ones(1,10);
+
+base1 = 5;
+R1_csch = 30;
+R1_csih = 20;
+R1_isch = 15;
+R1_isih = 10;
 
 A1_csch = base1 + R1_csch;
 A1_csih = base1 + R1_csih;
@@ -19,38 +25,56 @@ A1_isch = base1 + R1_isch;
 A1_isih = base1 + R1_isih;
 
 % 2 - inactivation
-base2 = base1 + 0;
-R2_csch = R1_csch*0.5;
-R2_csih = R1_csih*0.3;
+base2 = base1;
+R2_csch = R1_csch+100;
+R2_csih = R1_csih;
 R2_isch = R1_isch;
 R2_isih = R1_isih;
 
-A2_csch = base2 + R2_csch;
-A2_csih = base2 + R2_csih;
-A2_isch = base2 + R2_isch;
-A2_isih = base2 + R2_isih;
+baseline_multiplication_factor = 1; % typically is 1
+A2_csch = baseline_multiplication_factor*(base2 + R2_csch);
+A2_csih = baseline_multiplication_factor*(base2 + R2_csih);
+A2_isch = baseline_multiplication_factor*(base2 + R2_isch);
+A2_isih = baseline_multiplication_factor*(base2 + R2_isih);
+
+if 1 % for specific unit debug
+temp1 = num2cell([ 7.6667   11.1111    6.3333    4.1111]); % Lin_20170707_07
+temp2 = num2cell([32.4000   38.0000   24.6667   22.3333]); % Lin_20170707_07
+
+[A1_isch A1_csch A1_csih A1_isih] = deal(temp1{:});
+[A2_isch A2_csch A2_csih A2_isih] = deal(temp2{:});
+end
 
 
 map = jet(length(R1_csch));
 
-if 0 % normalize by max rate in condtion 1 in each cell
-	max_per_cell = max([A1_isch; A1_csch; A1_csih; A1_isih]);
-	A1_isch = A1_isch./max_per_cell;
-	A1_csch = A1_csch./max_per_cell;
-	A1_csih = A1_csih./max_per_cell;
-	A1_isih = A1_isih./max_per_cell;
+if 1 % normalize by max rate in condition 1 in each cell
+	max_per_cell_1 = max([A1_isch; A1_csch; A1_csih; A1_isih]);
+	% max_per_cell_1 = max([(A1_isch+A2_isch)/2; (A1_csch+A2_csch)/2; (A1_csih+A2_csih)/2; (A1_isih+A2_isih)/2]);
 	
-	A2_isch = A2_isch./max_per_cell;
-	A2_csch = A2_csch./max_per_cell;
-	A2_csih = A2_csih./max_per_cell;
-	A2_isih = A2_isih./max_per_cell;
+	A1_isch = A1_isch./max_per_cell_1;
+	A1_csch = A1_csch./max_per_cell_1;
+	A1_csih = A1_csih./max_per_cell_1;
+	A1_isih = A1_isih./max_per_cell_1;
+	
+	A2_isch = A2_isch./max_per_cell_1;
+	A2_csch = A2_csch./max_per_cell_1;
+	A2_csih = A2_csih./max_per_cell_1;
+	A2_isih = A2_isih./max_per_cell_1;
 	
 end
 
-
-for k=1:length(A1_isch),
+figure;
+for k=1 % 1:length(A1_isch),
 	plot_diag_two_conditions([1 2 3 4],[A1_isch(k) A1_csch(k) A1_csih(k) A1_isih(k)],...
 				 [1 2 3 4],[A2_isch(k) A2_csch(k) A2_csih(k) A2_isih(k)],'Marker','.','Color',map(k,:)); hold on
+			 
+			 % tuning indices
+			 TI1_space(k) = csi(mean([A1_csch A1_csih]),mean([A1_isch A1_isih]));
+			 TI2_space(k) = csi(mean([A2_csch A2_csih]),mean([A2_isch A2_isih]));
+			 TI1_hand(k) = csi(mean([A1_csch A1_isch]),mean([A1_isih A1_csih]));
+			 TI2_hand(k) = csi(mean([A2_csch A2_isch]),mean([A2_isih A2_csih]));
+			 
 end
 
 max1 = max(max([A1_isch; A1_csch; A1_csih; A1_isih]));
@@ -66,6 +90,18 @@ xlim = get(gca,'xlim');
 ylim = get(gca,'ylim');
 line([xlim(1) xlim(2)],[ylim(2) ylim(1)],'Color','k','LineStyle',':');
 colorbar;
+
+if length(A1_csch) == 1, % only one cell
+	figure;
+	plot(TI1_space,TI2_space,'ro'); hold on;
+	plot(TI1_hand,TI2_hand,'go'); hold on;
+	set(gca,'xlim',[-1 1], 'ylim', [-1 1]);
+	ig_add_diagonal_line;
+	ig_add_zero_lines;
+	axis square
+	xlabel('TI1');
+	ylabel('TI2');
+end
 
 function h = plot_diag_two_conditions(diag1, mag1, diag2, mag2, varargin)
 % diag 1 -> 2 -> 3 - 4 counterclockwise
@@ -109,8 +145,16 @@ h2 = plot([x2 x2(1)],[y2 y2(1)],'k-',varargin{:}); hold on;
 
 set(h2,'Color',get(h1,'Color')*0.66);
 
-[cx1,cy1] = ig_comass(x1,y1); 
-[cx2,cy2] = ig_comass(x2,y2); 
+% use center of mass
+% [cx1,cy1] = ig_comass(x1,y1); 
+% [cx2,cy2] = ig_comass(x2,y2); 
+
+% use vector sum
+cx1 = sum(x1);
+cy1 = sum(y1);
+cx2 = sum(x2);
+cy2 = sum(y2);
+
 
 if ~(cx1 == 0 && cy1 == 0),
 	% hf1 = feather(cx1,cy1,'r');
@@ -128,8 +172,9 @@ end
 drawArrow([cx1 cy1],[cx2 cy2],get(h1,'Color'));
 
 
-function h = plot_diag(diag, mag, varargin)
 
+function h = plot_diag(diag, mag, varargin)
+% OBSOLETE
 % diag 1 -> 2 -> 3 - 4 counterclockwise
 % IS-CH CS-CH CS-IH IS-IH
 
