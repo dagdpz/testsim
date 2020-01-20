d02de09a1ba215ea5ad200bf48926f5efa49375e
KristinKaduk <kadukkristin@gmail.com>
2020-01-20 17:49:43 +0100

    added graph and case

--- testsim_distractor_LG_KK_SDT.m
+++ testsim_distractor_LG_KK_SDT.m
@@ -15,6 +15,7 @@ scenario = 'add spatial bias to contra and ipsi';
 scenario = 'contra perceptual problem';
 scenario = 'high hit rate, but ipsi spatial bias'; % like Curius early stim single targets, difficult distractor
 scenario = 'DoubleStimuli add ipsi choice bias';
+scenario = 'Double Stimuli - Curius inactivation'; 
 
 % Enter the Proportion for Hits, Misses, FA, CR
 switch scenario
@@ -159,39 +160,51 @@ switch scenario
         M(4)   = M(3);
         FA(4)  = FA(2)+ sb;
         CR(4)  = CR(3);
-  case 'Double Stimuli - Curius inactivation ';
+  case 'Double Stimuli - Curius inactivation';
         % Fixation is the same for contra vs ipsi M(1) = M(2)
         % Pre: no choice bias -> Post: ipsi choice bias -> fixations doesn't change for both hemifields
         % target are highly selected
         % H(1) + M(1) + H(2) should add to 1
-        %pre 26/68 + 1/68  + 40/68 
+        %pre 26/67 + 1/67  + 40/67 
         %pst 11/61 + 0/61 +50/61
         % FA(1) + CR(1) + FA(2) should add to 1
-        H(1)   = 26/68;
-        M(1)   = 1/68;
-        FA(1)  = 0.2;
-        CR(1)  = 0.6;
+        H(1)   = 26/67;
+        M(1)   = 1/67;
+        FA(1)  = 21/65;
+        CR(1)  = 23/65;
         
-        H(2)   = 40/68;
+        H(2)   = 40/67;
         M(2)   = M(1);
-        FA(2)  = 0.2;
+        FA(2)  = 21/65;
         CR(2)  = CR(1);
         
 
         H(3)   = 11/61 ;
         M(3)   = 0/61;
-        FA(3)  = FA(1)- sb;
-        CR(3)  = CR(1);
+        FA(3)  = 19/62;
+        CR(3)  = 19/62;
         
         H(4)   = 50/61 ;
         M(4)   = M(3);
-        FA(4)  = FA(2)+ sb;
-        CR(4)  = CR(3);      
-        
+        FA(4)  = 24/62;
+        CR(4)  = CR(3);  
+        if H(1)+ M(1)+ H(2) == 1
+            disp('target-trials: add up to 1')
+        end
+        if FA(1)+ CR(1)+ FA(2) == 1
+            disp('distractor-trials: add up to 1')
+        end
 end
 
 
-
+%% It should be before the dprime calculation because 0.5 is added to the Nb.of trials
+c = 0;
+for indPos = 1:2
+Tar_IpsiSelection(1+c)    = H(1+indPos) ./ (H(1+indPos) + H(2+indPos) + M(1+indPos));
+Tar_ContraSelection(1+c)  = H(2+indPos) ./ (H(1+indPos) + H(2+indPos) + M(1+indPos));
+Tar_fixation(1+c)         = M(1+indPos) ./ (H(1+indPos) + H(2+indPos) + M(1+indPos));
+c = 0+1; 
+end
 
 % avoid 0 or Inf probabilities
 if any(H==0) || any(M==0) || any(FA==0) || any(CR==0),
@@ -228,23 +241,93 @@ pHit = H ./ (H + M);
 pFA = FA ./ (FA + CR);
 
 
+
 for k = 1:4,
-    [d(k),beta(k),c(k)] = testsim_dprime(pHit(k),pFA(k));
+    [dprime(k),beta(k),criterion(k)] = testsim_dprime(pHit(k),pFA(k));
 end
 
 %% Graph
 % Selection 
-% Hitrate vs Falsealarm rate
+figure('Position',[200 200 1200 900],'PaperPositionMode','auto'); % ,'PaperOrientation','landscape'
+set(gcf,'Name','Selection');
+    subplot(1,3,1);
+    plot([1;2], [Tar_IpsiSelection(1),Tar_IpsiSelection(2)], 'o','color',[0 0 0] , 'MarkerSize',15,'markerfacecolor',[0 0 0 ]); hold on;
+    line([1;2], [Tar_IpsiSelection(1),Tar_IpsiSelection(2)], 'Color',[0 0 0],'LineWidth', 2); hold on;
+    set(gca,'ylim',[0 1])
+    ylabel( 'Ipsilateral selection','fontsize',14,'fontweight','b', 'Interpreter', 'none' );
+    set(gca,'xlim',[0 3],'Xtick',1:2,'XTickLabel',{'pre' 'post'},'fontsize',20);
+
+    subplot(1,3,2);
+    plot([1;2], [Tar_ContraSelection(1),Tar_ContraSelection(2)], 'o','color',[0 0 0] , 'MarkerSize',15,'markerfacecolor',[0 0 0 ]); hold on;
+    line([1;2], [Tar_ContraSelection(1),Tar_ContraSelection(2)], 'Color',[0 0 0],'LineWidth', 2); hold on;
+    set(gca,'ylim',[0 1])
+    ylabel( 'Contralateral Selection','fontsize',14,'fontweight','b', 'Interpreter', 'none' );
+    set(gca,'xlim',[0 3],'Xtick',1:2,'XTickLabel',{'pre' 'post'},'fontsize',20);
+
+    subplot(1,3,3);
+    plot([1;2], [Tar_fixation(1),Tar_fixation(2)], 'o','color',[0 0 0] , 'MarkerSize',15,'markerfacecolor',[0 0 0 ]); hold on;
+    line([1;2], [Tar_fixation(1),Tar_fixation(2)], 'Color',[0 0 0],'LineWidth', 2); hold on;
+    set(gca,'ylim',[0 1])
+    ylabel( 'Fixation','fontsize',14,'fontweight','b', 'Interpreter', 'none' );
+    set(gca,'xlim',[0 3],'Xtick',1:2,'XTickLabel',{'pre' 'post'},'fontsize',20);
+
+%% Hitrate vs Falsealarm rate
+figure('Position',[200 200 1200 900],'PaperPositionMode','auto'); % ,'PaperOrientation','landscape'
+set(gcf,'Name','Hitrate and FalseAlarmRate');
+
+    subplot(2,3,1);
+    plot([1;2], [pHit(2),pHit(4)], 'o','color',[0 0 0] , 'MarkerSize',15,'markerfacecolor',[0 0 0 ]); hold on;
+    line([1;2], [pHit(2),pHit(4)], 'Color',[0 0 0],'LineWidth', 2); hold on;
+    set(gca,'ylim',[0 1])
+    ylabel( 'Hitrate','fontsize',14,'fontweight','b', 'Interpreter', 'none' );
+    set(gca,'xlim',[0 3],'Xtick',1:2,'XTickLabel',{'pre' 'post'},'fontsize',20);
+    title('ipsi')
+
+    subplot(2,3,4);
+    plot([1;2], [pHit(1),pHit(1)], 'o','color',[0 0 0] , 'MarkerSize',15,'markerfacecolor',[0 0 0 ]); hold on;
+    line([1;2], [pHit(1),pHit(1)], 'Color',[0 0 0],'LineWidth', 2); hold on;
+    set(gca,'ylim',[0 1])
+    ylabel( 'Hitrate','fontsize',14,'fontweight','b', 'Interpreter', 'none' );
+    set(gca,'xlim',[0 3],'Xtick',1:2,'XTickLabel',{'pre' 'post'},'fontsize',20);
+    title('contra')
+
+    subplot(2,3,3);
+    plot([1;2], [pFA(2),pFA(4)], 'o','color',[0 0 0] , 'MarkerSize',15,'markerfacecolor',[0 0 0 ]); hold on;
+    line([1;2], [pFA(2),pFA(4)], 'Color',[0 0 0],'LineWidth', 2); hold on;
+    set(gca,'ylim',[0 1])
+    ylabel( 'Hitrate','fontsize',14,'fontweight','b', 'Interpreter', 'none' );
+    set(gca,'xlim',[0 3],'Xtick',1:2,'XTickLabel',{'pre' 'post'},'fontsize',20);
+    title('ipsi')
+    
+    subplot(2,3,6);
+    plot([1;2], [pFA(1),pFA(3)], 'o','color',[0 0 0] , 'MarkerSize',15,'markerfacecolor',[0 0 0 ]); hold on;
+    line([1;2], [pFA(1),pFA(3)], 'Color',[0 0 0],'LineWidth', 2); hold on;
+    set(gca,'ylim',[0 1])
+    ylabel( 'Hitrate','fontsize',14,'fontweight','b', 'Interpreter', 'none' );
+    set(gca,'xlim',[0 3],'Xtick',1:2,'XTickLabel',{'pre' 'post'},'fontsize',20);
+    title('contra')
+
+
 % Dprime vs Criterion
 figure('Position',[200 200 1200 900],'PaperPositionMode','auto'); % ,'PaperOrientation','landscape'
 set(gcf,'Name','Dprime vs Criterion');
-for indPos = 1:length(positionExp)
-    ha(indPos) = subplot(2,2,(indPos *2)-1);
-    plot([d_prime.easy.(positionExp{indPos}).pre],mult*criterion.easy.(positionExp{indPos}).pre, 'o','color',[0 0 0]   , 'MarkerSize',10,'markerfacecolor',[1 1 1 ]); hold on;
-    plot([d_prime.easy.(positionExp{indPos}).post],mult*criterion.easy.(positionExp{indPos}).post, 'o','color',[0 0 1] ,'MarkerSize',10,'markerfacecolor',[1 1 1]);
-    
-    plot(nanmean([d_prime.easy.(positionExp{indPos}).pre]),mult*nanmean([criterion.easy.(positionExp{indPos}).pre]), 'o','color',[0 0 0] ,'Linewidth',2,'MarkerSize',18,'markerfacecolor',criterion.easy.(positionExp{indPos}).Bias_pre_Color);
-    plot(nanmean([d_prime.easy.(positionExp{indPos}).post]),mult*nanmean([criterion.easy.(positionExp{indPos}).post]), 'o','color',[0 0 1],'Linewidth',2,'MarkerSize',18,'markerfacecolor',criterion.easy.(positionExp{indPos}).Bias_post_Color);
+c = 0;
+for indPos = 1:2
+    subplot(1,2,indPos);
+    plot(dprime(1+c),criterion(1+c), 'o','color',[0 0 0] , 'MarkerSize',15,'markerfacecolor',[0 0 0 ]); hold on;
+    plot(dprime(3+c),criterion(3+c), 'o','color',[0 0 1] ,'MarkerSize',15,'markerfacecolor',[0 0 1]);
+    xlabel('sensitivity')
+    ylabel('criterion')
+    set(gca,'ylim',[-2 2])
+    set(gca,'xlim',[0 4])
+    if indPos == 1
+        title('contra')
+    else
+        title('ipsi')
+    end
+    c = c+1;
+   legend('pre', 'post')
+
 end
 
 
@@ -254,14 +337,14 @@ set(gcf,'Color',[1 1 1]);
 
 subplot(2,1,1)
 
-plot([1 3],[d(1) d(3)],'ro-','MarkerFaceColor',[1 0 0]); hold on
-plot([2 4],[d(2) d(4)],'bo-','MarkerFaceColor',[0 0 1]); hold on
+plot([1 3],[dprime(1) dprime(3)],'ro-','MarkerFaceColor',[1 0 0]); hold on
+plot([2 4],[dprime(2) dprime(4)],'bo-','MarkerFaceColor',[0 0 1]); hold on
 set(gca,'Xlim',[0 5],'Xtick',[1:4],'XtickLabel',{'contra pre' 'ipsi pre' 'contra post' 'ipsi post'},'FontSize',12,'TickDir','out','box','off');
 title([scenario sprintf('\n') 'dprime'])
 
 subplot(2,1,2)
-plot([1 3],[c(1) c(3)],'ro-','MarkerFaceColor',[1 0 0]); hold on
-plot([2 4],[-c(2) -c(4)],'bo-','MarkerFaceColor',[0 0 1]); hold on % reverse direction of criterion for ipsi
+plot([1 3],[criterion(1) criterion(3)],'ro-','MarkerFaceColor',[1 0 0]); hold on
+plot([2 4],[-criterion(2) -criterion(4)],'bo-','MarkerFaceColor',[0 0 1]); hold on % reverse direction of criterion for ipsi
 plot([0 5],[0 0],'k--');
 set(gca,'Xlim',[0 5],'Xtick',[1:4],'XtickLabel',{'contra pre' 'ipsi pre' 'contra post' 'ipsi post'},'FontSize',12,'TickDir','out','box','off');
 title(['criterion'])
