fd53e1f77e755d72b871059fe5d9dc8e4f5fd0a2
Igor Kagan <igor.b.kagan@gmail.com>
2020-05-05 12:10:35 +0200

    Update testsim_trial_seq_prob.m

--- testsim_trial_seq_prob.m
+++ testsim_trial_seq_prob.m
@@ -6,17 +6,17 @@
 
 % Examples, all assume L = 1, R = 2
 
-% 1 Completely random 
+% 1. Completely random 
 % L_prob = 0.72;
 % n_trials = 100;
 % seq = randsample([1 2],n_trials,true,[L_prob 1-L_prob]);
 
-% 2
+% 2.
 % seq = [1 2 2 2 2 2 2 2 2 2 1 2 2 2 2 2 2 2 2 2 2]; n_trials = length(seq);
 % prob. of 1 preceded by 1 = 0; prob. of 1 preceded by 2 = 1; prob. of 2 preceded by 1 = 0.1; prob. of 2 preceded by 2 = 0.9; 
 % prob. of 1 followed by 1 = 0; prob. of 1 followed by 2 = 1; prob. of 2 followed by 1 = .06; prob. of 2 followed by 2 = 0.94;
 
-% 3
+% 3.
 % seq = [1 1 2 2 2 2 2 2 2 2 2 1 1 2 2 2 2 2 2 2 2 2 2]; n_trials = length(seq);
 % prob. of 1 (current) preceded by 1 (preceding) = 0.67; prob. of 1 (current) preceded by 2 = 0.33;
 % prob. of 2 (current) preceded by 1 (preceding) = 0.1; prob. of 2 (current) preceeded by 2 = 0.9;
@@ -29,7 +29,7 @@
 % P(1|2) 0.06	P(2|2) 0.94
 % P(next|current)
 
-% 4 Markov chain of 1st order
+% 4. Markov chain of 1st order
 % n_trials = 10000;
 % TM = [0.1 0.9; 0.8 0.2]; % transition matrix
 % seq = testsim_markov_chain(n_trials,TM,1); % states 1 and 2
@@ -44,7 +44,7 @@
 % P = TM - [1 0; 0 1];
 % q1 = -P(2,1)/(P(1,1)-P(2,1)) % L_prob_theoretical
 
-% 5 Markov chain of 1st order, equal transitions
+% 5. Markov chain of 1st order, equal transitions
 n_trials = 10000;
 TM = [0.25 0.75; 0.25 0.75]; % transition matrix
 seq = testsim_markov_chain(n_trials,TM,1); % states 1 and 2
@@ -62,4 +62,11 @@ out = ig_analyze_trial_sequence('seq',seq,'condition_labels',{{'L' 'R'}},'group_
 out.Ppc
 out.Pnc
 
+% https://en.wikipedia.org/wiki/Approximate_entropy
 % apen = ApEn(2,0.2*std(seq),seq)
+
+% https://en.wikipedia.org/wiki/Sample_entropy
+SampEn = sampen(seq,1,0.2*std(seq))
+
+% runs test https://en.wikipedia.org/wiki/Wald%E2%80%93Wolfowitz_runs_test
+[H_runs,p_runs] = runstest(seq)
