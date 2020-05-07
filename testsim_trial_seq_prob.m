91c075c7442696c96d8b89587711bd53baa1f9dc
Igor Kagan <igor.b.kagan@gmail.com>
2020-05-07 18:43:12 +0200

    trial_seq_prob and markov_chain updates

--- testsim_trial_seq_prob.m
+++ testsim_trial_seq_prob.m
@@ -2,7 +2,10 @@
 % Given a choice bias, what is probability one choice (L,1) is preceded/followed by another (R,2)?
 % "the conditional probability of A given B" or "the probability of A under the condition B"
 % P(A|B), or P_B(A)
-
+% 
+% More info:
+% https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2801541/
+% \papers\reward decision\Nickerson - 2002 - The production and perception of randomness.pdf
 
 % Examples, all assume L = 1, R = 2
 
@@ -40,23 +43,28 @@
 %		0.1	0.9
 %		0.8	0.2
 % Theoretical P(1) and P(2) (let's call them q1 and q2)
-% See https://en.wikipedia.org/wiki/Examples_of_Markov_chains > A simple weather model
+% See https://en.wikipedia.org/wiki/Examples_of_Markov_chains#A_simple_weather_model
 % P = TM - [1 0; 0 1];
 % q1 = -P(2,1)/(P(1,1)-P(2,1)) % L_prob_theoretical
+% q1 = -TM(2,1)/(TM(1,1)-1-TM(2,1)) % L_prob_theoretical
 
-% 5. Markov chain of 1st order, equal transitions
-n_trials = 10000;
-TM = [0.25 0.75; 0.25 0.75]; % transition matrix
+% 5. Markov chain of 1st order, equal transitions: meaning that L and R are fully independent
+n_trials = 1000;
+TM = [0.25 0.75; 0.25 0.75]; % independent L-R transition matrix
+% OR (for contrast)
+TM = [0.1 0.9; 0.3 0.7]; % transition matrix where prob. or L (1) is increasing after R (2), and prob. of R is increasing after L
 seq = testsim_markov_chain(n_trials,TM,1); % states 1 and 2
 % P = TM - [1 0; 0 1];
 % q1 = -P(2,1)/(P(1,1)-P(2,1)) % L_prob_theoretical
+% q1 = -TM(2,1)/(TM(1,1)-1-TM(2,1)) % L_prob_theoretical
 
 
 % output
 L_prob_actual = length(find(seq==1))/n_trials
 
-param.conditions_compare_vs_LR	= [1 2 3]; % condtions to compare to preceeding/next L or R conditions
-param.group_LR			= {[1 3 5] [2 4 6]}; % two groups, one left and one right (can also include conditions_compare_vs_LR) 
+% param.conditions_compare_vs_LR	= [1 2 3]; % condtions to compare to preceeding/next L or R conditions
+% param.group_LR			= {[1 3 5] [2 4 6]}; % two groups, one left and one right (can also include conditions_compare_vs_LR) 
+
 out = ig_analyze_trial_sequence('seq',seq,'condition_labels',{{'L' 'R'}},'group_conditions',{{[1 2]}},'conditions_compare_vs_LR',[1 2],'group_LR',{{[1] [2]}});
 
 out.Ppc
