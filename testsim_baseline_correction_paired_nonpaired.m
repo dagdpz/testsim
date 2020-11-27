0c695b6af2d849e61270b3d0dbb8443ca0a653a7
Igor Kagan <igor.b.kagan@gmail.com>
2020-11-27 13:56:41 +0100

    Update testsim_baseline_correction_paired_nonpaired.m

--- testsim_baseline_correction_paired_nonpaired.m
+++ testsim_baseline_correction_paired_nonpaired.m
@@ -1,23 +1,49 @@
 % testsim_baseline_correction_paired_nonpaired
+close
 
 N_trials = 20; % number of trials
 noise_trial2trial_sd = 10; % trial to trial noise, same for the baseline and response
+b_mean = 0;
 response_mean = 1;
 response_sd = 1;
-
-b = normrnd(0,noise_trial2trial_sd,[N_trials,1]);
-r = b + normrnd(response_mean,response_sd,[N_trials,1]);
-
-if 1 % baseline correction
-    b = b - b;
-    r = r - b;
     
-    % NOTE: baseline correction invalidates the "pairing"!
-end
-
-if 0 % absolute
-    b = abs(b);
-    r = abs(r);
+if 0 % simulate average response amplitude
+    
+    b = normrnd(b_mean,noise_trial2trial_sd,[N_trials,1]);
+    r = b + normrnd(response_mean,response_sd,[N_trials,1]);
+    
+    if 0 % baseline correction
+        b = b - b;
+        r = r - b;
+        
+        % NOTE: baseline correction invalidates the "pairing"!
+    end
+    
+    if 0 % absolute
+        b = abs(b);
+        r = abs(r);
+    end
+    
+else % simulate timecourses
+    n_samples = 100; % per epoch
+    btt = repmat(normrnd(b_mean,noise_trial2trial_sd,[N_trials,1]),1,n_samples);
+    bt = btt + normrnd(b_mean,1,[N_trials,n_samples]); % trial x sample
+    rt = btt + normrnd(response_mean,response_sd,[N_trials,n_samples]);
+    
+    if 1 % baseline correction
+        bc = repmat(mean(bt,2),1,n_samples);
+        bt = bt - bc;
+        rt = rt - bc;
+    end
+    
+    if 1 % absolute
+        bt = abs(bt);
+        rt = abs(rt);
+    end
+    
+    b = mean(bt,2);
+    r = mean(rt,2);
+    
 end
 
 plot([1 2],[b r],'ko'); hold on
