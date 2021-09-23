ab7d2bdf920e8db82934fc85c18c44356f95dc82
Igor Kagan <igor.b.kagan@gmail.com>
2021-09-23 12:42:01 +0200

    Update testsim_instr_choice_FR.m

--- testsim_instr_choice_FR.m
+++ testsim_instr_choice_FR.m
@@ -2,11 +2,11 @@ function testsim_instr_choice_FR
 % response amplitudes for instr and choice trials
 % null hypothesis: just noise (no tuning)
 noise_level = [0.5:0.5:20];
-N_rep = 1;
+N_rep = 20;
 
 for n = 1:length(noise_level),
 	for r = 1:N_rep,
-		[p(n,r) CII(n,r)] = testsim_instr_choice_FR_one_noise_level(noise_level(n), 1);
+		[p(n,r) CII(n,r)] = testsim_instr_choice_FR_one_noise_level(noise_level(n), 0);
 	end
 end	
 
@@ -23,7 +23,7 @@ ylabel('CII');
 end
 
 
-function [p meanCII meanCCI] = testsim_instr_choice_FR_one_noise_level(noise_level, TOPLOT)
+function [p, meanCII, meanCCI] = testsim_instr_choice_FR_one_noise_level(noise_level, TOPLOT)
 n_trials = 30; % per hemifield
 %noise_level = 5;
 
@@ -33,24 +33,29 @@ for k = 1:50, % units
 	FR_i_r = unit_FR_level + noise_level*randn(1,n_trials);
 	FR_c_l = unit_FR_level + noise_level*randn(1,n_trials);
 	FR_c_r = unit_FR_level + noise_level*randn(1,n_trials);
+    
+    trials1 = [1:n_trials/2]; % trials for instr tuning
+    trials2 = [n_trials/2+1:n_trials]; % trials for CII 
+    
 	
-	if mean(FR_i_l) > mean(FR_i_r),
-		RA_i(k) = mean(FR_i_l);
-		RA_c(k) = mean(FR_c_l);
-		RA_cu(k) = mean(FR_c_r);
+	if mean(FR_i_l(trials1)) > mean(FR_i_r(trials1)),
+		RA_i(k) = mean(FR_i_l(trials2)); % pref. instr
+		RA_c(k) = mean(FR_c_l); % pref. choice
+		RA_cu(k) = mean(FR_c_r); % non-pref. choice
 	else
-		RA_i(k) = mean(FR_i_r);
+		RA_i(k) = mean(FR_i_r(trials2));
 		RA_c(k) = mean(FR_c_r);
 		RA_cu(k) = mean(FR_c_l);
 	end
-	CII(k) = 100*(RA_c(k) - RA_i(k))./max([RA_i(k) RA_c(k)]); 
-	CCI(k) = 100*(RA_c(k) - RA_cu(k))./max([RA_c(k) RA_cu(k)]);
+	CII(k) = 100*(RA_c(k) - RA_i(k))./max([RA_i(k) RA_c(k)]); % Choice to Instr index (if negative, there is a suppression due to double targets)
+	CCI(k) = 100*(RA_c(k) - RA_cu(k))./max([RA_c(k) RA_cu(k)]); % Choice selectivity 
 	
 	
 	
 end
 [h,p] = ttest(RA_i,RA_c);
 meanCII = mean(CII);
+meanCCI = mean(CCI);
 
 if TOPLOT,
 	figure('Position',[300 300 500 900]);
@@ -74,7 +79,7 @@ if TOPLOT,
 	axis square
 	ig_add_equality_line;
 	
-	corrcoef_eval(CII,CCI);
+	corrcoef(CII,CCI);
 	
 end
 
