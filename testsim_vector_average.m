6ebd0f0360ecd92cdd3e7db1a82b8c899738e149
Igor Kagan <igor.b.kagan@gmail.com>
2023-08-09 17:44:12 +0200

    Create testsim_vector_average.m

---
+++ testsim_vector_average.m
@@ -0,0 +1,35 @@
+% function [resultant_r, resultant_theta] = testsim_vector_average(thetas)
+% thetas = 2 * pi * rand(1, 100);
+% testsim_vector_sum(thetas)
+
+% Number of vectors
+N = length(thetas);
+
+% Convert each unit vector from polar to Cartesian
+x = cos(thetas);
+y = sin(thetas);
+
+% Sum up all the x's and y's, divide by N
+resultant_x = sum(x)/N;
+resultant_y = sum(y)/N;
+
+% Convert the resultant vector from Cartesian to polar
+resultant_r = sqrt(resultant_x^2 + resultant_y^2);
+resultant_theta = atan2(resultant_y, resultant_x);
+
+% use circ_stats
+[mu, ul, ll] = circ_mean(thetas);
+r = circ_r(thetas);
+
+% Plotting in polar coordinates
+figure;
+subplot(1,2,1);
+circ_plot(thetas,'hist',[],20,true,true,'linewidth',2,'color','r')
+
+subplot(1,2,2);
+% Plot unit vectors
+compass(x,y, '-b');
+hold on;
+
+% Plot resultant vector
+compass(resultant_x, resultant_y, '-r'); % Plot resultant vector in red
