3a8966509201755df6ca29fd556b514ebe5b0f6d
lubavas <19555010+lubavas@users.noreply.github.com>
2023-10-23 10:36:12 +0200

    added explanations

--- testsim_rpeak_interval_jittering_distributions.m
+++ testsim_rpeak_interval_jittering_distributions.m
@@ -1,8 +1,17 @@
-a=3; b=2;
-x=-20:20;
-N=10000;
+% This simulation is created to study distribution changes in the jittering
+% procedure implemented in ecg_bna repo.
+%
+% GAMMADISTRIBUTION - simulates a skewed distribution of RR-intervals in
+% ECG
+% JITTERED_DISTRIBUTION - shows what happens to the original distribution
+% after jittering
+%
 
-gammadistribution =gamrnd(a,b,1,N);
+k=3; theta=2; % parameters of a gamma distribution: k - shape parameter, theta - scale parameter
+x=-20:20; % bins
+N=10000; % sample size
+
+gammadistribution =gamrnd(k,theta,1,N);
 gamhist=hist(gammadistribution,x);
 
 jittered_distribution=gammadistribution+randn(1,N)*std(gammadistribution);
@@ -12,3 +21,4 @@ figure
 hold on;
 plot(x,gamhist)
 plot(x,jittered_hist)
+legend({'Original Distribution', 'Jittered Distribution'})
